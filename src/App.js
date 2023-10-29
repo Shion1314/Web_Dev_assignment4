1272ad03b5f10b221b8ffb58e413b21d00f2f6fe
alexy4744 <alexy4744@gmail.com>
2023-10-29 18:21:43 -0400

    allow user to color cells using value from color picker

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import { useRef, useState } from "react";
+import { HexColorInput, HexColorPicker } from "react-colorful";
+import { Popover } from "react-tiny-popover";
 
 import "./styles.css";
 
@@ -7,6 +9,9 @@ const range = (n) => [...Array(n).keys()]; // equivalent to range() in python
 export default function App() {
   const [rows, setRows] = useState(0);
   const [cols, setCols] = useState(0);
+
+  const [color, setColor] = useState("#fff");
+  const [isPickerOpened, setIsPickerOpened] = useState(false);
 
   const addRow = () => {
     setRows((currentValue) => {
@@ -48,15 +53,39 @@ export default function App() {
     });
   };
 
+  const colorCell = (cell) => {
+    cell.style.backgroundColor = color;
+  };
+
   return (
     <div>
       <h1>Add Square to Blank Sheet</h1>
 
-      <div className="button-group">
-        <button onClick={addRow}>Add Row</button>
-        <button onClick={removeRow}>Remove Row</button>
-        <button onClick={addColumn}>Add Column</button>
-        <button onClick={removeColumn}>Remove Column</button>
+      <div className="flex-row">
+        <div className="button-group">
+          <button onClick={addRow}>Add Row</button>
+          <button onClick={removeRow}>Remove Row</button>
+          <button onClick={addColumn}>Add Column</button>
+          <button onClick={removeColumn}>Remove Column</button>
+        </div>
+
+        <Popover
+          isOpen={isPickerOpened}
+          padding={10}
+          positions={["bottom"]}
+          content={
+            <>
+              <HexColorPicker color={color} onChange={setColor} />
+              <HexColorInput color={color} onChange={setColor} prefixed />
+            </>
+          }
+        >
+          <button
+            className="swatch"
+            onClick={() => setIsPickerOpened(!isPickerOpened)}
+            style={{ backgroundColor: color }}
+          />
+        </Popover>
       </div>
 
       <table id="grid-sheet">
@@ -64,7 +93,11 @@ export default function App() {
           {range(rows).map((key) => (
             <tr key={key}>
               {range(cols).map((key) => (
-                <td className="square" key={key} />
+                <td
+                  key={key}
+                  className="square"
+                  onClick={({ currentTarget }) => colorCell(currentTarget)}
+                />
               ))}
             </tr>
           ))}
