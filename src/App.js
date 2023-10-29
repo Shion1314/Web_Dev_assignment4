662ce6534b5c7439620d0c0314ba3ada190358e2
Shion1314 <jiaxinlin200204@gmail.com>
2023-10-28 23:14:49 -0400

    Row function

--- src/App.js
+++ src/App.js
@@ -1,3 +1,54 @@
-export default function Square() {
-  return <button className="square">X</button>;
+import React, { useState } from 'react';
+import './styles.css';
+
+function Row({ NumCol}) {
+  const square = [];
+  for (let index = 0; index < NumCol; index++) {
+    square.push(<td key={index} className='square'></td>)
+  }
+
+  return (
+   <tr>{square}
+  
+   
+   </tr>
+  );
+}
+
+export default function App() {
+  const [grid, setGrid] = useState([]);
+
+  const addRow = () => {
+    let NumCol;
+    if (grid[0]) { 
+      NumCol = grid[0].props.NumCol;
+    } else {
+      NumCol = 1;
+    }
+    const newRow = <Row NumCol={NumCol} />;
+    const row = [...grid, newRow]
+    setGrid(row);
+  };
+
+  const removeRow = () => {
+    if (grid.length > 0) {
+      setGrid(grid.slice(0, -1));
+    }
+  };
+
+
+
+
+  return (
+    <div>
+      <h1>Add Square to Blank Sheet</h1>
+      <div className="button-group">
+        <button onClick={addRow}>Add Row</button>
+        <button onClick={removeRow}>Remove Row</button>
+      </div>
+      <table id="grid-sheet">
+        <tbody>{grid}</tbody>
+      </table>
+    </div>
+  );
 }
